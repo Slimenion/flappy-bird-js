1679585fcfc186076179fab71001bc0e615dacf8
Maxim Bobrovskii <m.bobrovskij2020@gmail.com>
2021-08-03 12:56:24 +0300

    Added movement bird

--- js/game.js
+++ js/game.js
@@ -14,3 +14,34 @@ bg.src = "img/background.png";
 fg.src = "img/foreground.png";
 pipeUp.src = "img/pipeUp.png";
 pipeBottom.src = "img/pipeBottom.png";
+
+var gap = 90;
+
+//При нажатии на кнопку
+document.addEventListener("keyup", moveUp);
+document.addEventListener("click", moveUp);
+
+function moveUp() {
+    yPos -= 20;
+}
+
+//Позиция птички
+var xPos = 10;
+var yPos = 150;
+var grav = 1;
+
+//Отрисовываем игру
+function drawGame() {
+    ctx.drawImage(bg, 0, 0);
+
+    ctx.drawImage(pipeUp, 100, 0);
+    ctx.drawImage(pipeBottom, 100, 0 + pipeUp.height + gap);
+
+    ctx.drawImage(fg, 0, cvs.height - fg.height);
+    ctx.drawImage(bird, xPos, yPos);
+
+    yPos += grav;
+    requestAnimationFrame(drawGame);
+}
+
+pipeBottom.onload = drawGame;
